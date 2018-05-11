f48e0573a4148b3529558144487b6b1840203bbd
kevinkorte <kevinkorte15@gmail.com>
2018-05-10 21:15:35 -0700

    the next pages

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,4 +1,8 @@
+import '../../ui/layouts/App_home.html';
 import "../../ui/pages/main.js";
+import '../../ui/pages/adminNav.js';
+import '../../ui/pages/billing.html';
+
 
 FlowRouter.route('/signup', {
   name: "signup",
@@ -7,9 +11,18 @@ FlowRouter.route('/signup', {
   }
 });
 
-FlowRouter.route('/signup', {
-  name: "onboard",
+FlowRouter.route('/dashboard', {
+  name: "dashboard",
   action: function () {
-    console.log("success");
+    BlazeLayout.render("App.home");
+  }
+});
+
+FlowRouter.route('/dashboard/billing', {
+  name: "billing",
+  action: function () {
+    BlazeLayout.render("App.home", {
+      template: "billing"
+    });
   }
 })
