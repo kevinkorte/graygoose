6fb3ff8ec4317e0721b831d7f9d56e90dbba3eaf
kevinkorte <kevinkorte15@gmail.com>
2018-06-09 10:28:44 -0700

    push

--- imports/api/Email/templates.js
+++ imports/api/Email/templates.js
@@ -3,11 +3,12 @@ import { Accounts } from 'meteor/accounts-base'
 Accounts.emailTemplates.from = 'hello@realtap.com';
 Accounts.emailTemplates.siteName = 'RealTap';
 Accounts.emailTemplates.verifyEmail = {
-  subject() {
-    "Welcome! Email verification for RealTap"
+  subject(user) {
+    return 'Welcome ${user.profile.name.first}! Email verification for RealTap';
   },
   html(user, url) {
     SSR.compileTemplate('verificationHTML', Assets.getText('email_verification.html'));
-    return SSR.render('verificationHTML', user, url);
+    const data = Object.assign({url: url}, user);
+    return SSR.render('verificationHTML', data);
   }
 }
