8d58fccdcc2a7e60943d7fa1c6a3b2a57c39abf8
kevinkorte <kevinkorte15@gmail.com>
2018-06-11 22:01:02 -0700

    push

--- imports/ui/pages/New/new.js
+++ imports/ui/pages/New/new.js
@@ -1,3 +1,31 @@
-
+import { Template } from 'meteor/templating';
+import { Session } from 'meteor/session';
+import flatpickr from "flatpickr";
 
 import './new.html';
+
+Template.new.onRendered(() => {
+  let startDate = $('#start').flatpickr({
+    minDate: 'today',
+    altInput: true,
+    enableTime: true,
+    onChange: function(selectedDates, dateStr, instance) {
+      endDate.set('minDate', Date.parse(selectedDates[0]));
+      Session.set('startDate', selectedDates[0]);
+    }
+  });
+  let endDate = $('#end').flatpickr({
+    minDate: 'today',
+    altInput: true,
+    enableTime: true,
+    onChange: function(selectedDates, dateStr, instance) {
+      startDate.set('maxDate', Date.parse(selectedDates[0]));
+    }
+  });
+  let input = document.getElementById('address');
+  autocomplete = new google.maps.places.Autocomplete(input);
+  autocomplete.addListener('place_changed', () => {
+    let place = autocomplete.getPlace();
+    console.log(place);
+  })
+})
