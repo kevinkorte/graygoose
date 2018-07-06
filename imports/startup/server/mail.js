1961c8b0fc9e0288369efd4c13c2cd010072f36a
kevinkorte <kevinkorte15@gmail.com>
2018-07-05 20:49:55 -0700

    fixes #55

--- imports/startup/server/mail.js
+++ imports/startup/server/mail.js
@@ -1,5 +1,15 @@
 import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
 
 Meteor.startup(function() {
   process.env.MAIL_URL = Meteor.settings.private.emailSMTP;
 });
+
+Accounts.emailTemplates.siteName = "RealTAP";
+Accounts.emailTemplates.from = 'RealTAP <realtap@realtap.net>';
+
+Accounts.emailTemplates.resetPassword.text = (user, url) => {
+  return 'Hello ' + user.profile.name.first + '\n\n'
+  + 'To reset your password, simply click the link below. \n\n'
+  + url 
+}
